208432e1a82292390cf70152bbed0a60e9e00efc
Claud Pod <claudiapollinger.cp@gmail.com>
2019-08-23 18:42:59 -0600

    added files to the page and got ther server.js set up

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require(".models");
+const PORT = 3000;
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/mongohomeworkunit18", { useNewUrlParser: true });
+
+// ROutes 
+
+
+axios.get("https://www.vice.com/en_us").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    var results = [];
+
+    $(".heading").each(function (i, element) {
+        var title = $(element).text();
+
+        var link = $(element).children().attr("href");
+
+        results.push({
+            title: title,
+            link: link
+        });
+    });
+    console.log(results);
+})
+
+
+app.listen(PORT, function() {
+    console.log("App is running on port " + PORT + "!");
+  });
+  
