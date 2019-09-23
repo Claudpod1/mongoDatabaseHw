497430bdb45751981165adadc4a4ccbe2885460e
Claud Pod <claudiapollinger.cp@gmail.com>
2019-09-23 17:18:08 -0600

    got rid of materialize

--- controllers/index.js
+++ controllers/index.js
@@ -4,36 +4,56 @@ const router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-
-
 // route for a homepage 
 
-router.get("/", function (req, res){
+router.get("/", function (req, res) {
     res.render("index")
 })
 
-// module.exports = {
-//     scrape: function (req, res) {
-//         axios.get("https://www.vice.com/en_us").then(function (response) {
-//             var $ = cheerio.load(response.data);
+// route for scraping vice.com
+
+router.get("/scrape", function (req, res) {
+    axios.get("https://www.vice.com/en_us").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $(".heading-hover").each(function (i, element) {
+            var results = {};
+            results.title = $(element).text();
+
+            results.link = `www.vice.com'{$(element).attr("href")}`;
+
+            db.Article.create(results)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
 
 
+            // create a new article 
 
-//             $(".heading-hover").each(function (i, element) {
-//                 var results = {};
-//                 results.title = $(element).text();
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        router.get("/articles", function (req, res) {
+            db.Article.find({})
+                .populate('note')
+                .then(function (dbArticle) {
+                    let articleObject = { articles: dbArticle }
+                    res.render("index", articleObject)
+                })
+                .catch(function (err) {
+                    console.log(res.json(err))
+                });
 
-//                 results.link = `www.vice.com${$(element).attr("href")}`;
+            });
+        });
+    })
 
-//                 db.Article.create(results)
-//                     .then(function (dbArticle) {
-//                         console.log(dbArticle);
-//                     })
-//                     .catch(function (err) {
-//                         console.log(err);
-//                     });
-//             });
-
-//         })
-//     }
-// }
+    module.exports = router; 
